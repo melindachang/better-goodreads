facad7432a3447479a00f1157271b9714c92e09b
Melinda Chang <melindachang.hy@gmail.com>
2024-06-23 17:57:55 -0700

    initial: user page refacator

---
+++ src/components/Rating.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  export let rating: number;
+  let starStr: string = "";
+
+  onMount(() => {
+    for (let i = 0; i < rating; i++) starStr += "★";
+    starStr = " " + starStr + " ";
+  });
+</script>
+
+<span class="stars stars-{rating}">{starStr}</span>
+
+<style lang="sass">
+  .stars
+    font-size: 1.3rem
+    background: url(https://s.ltrbxd.com/static/img/sprite.6c5e9c43.svg) no-repeat -450px -50px
+    display: inline-block
+    height: 1.6rem
+    overflow: hidden
+    text-indent: 110%
+    position: relative
+    white-space: nowrap
+    &-1
+      width: 1.2rem
+    &-2
+      width: 2.5rem
+    &-3
+      width: 3.8rem
+    &-4
+      width: 5.1rem
+    &-5
+      width: 6.4rem
+</style>
---
+++ src/components/Shelf.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  export let href: string, name: string | null;
+</script>
+
+<a class="shelf" {href}> {name} </a>
+
+<style lang="sass">
+  @use '../sass/variables' as variables
+
+  .shelf
+    background-image: url(https://s.ltrbxd.com/static/img/sprite.6c5e9c43.svg)
+    background-position: right -810px
+    background-repeat: no-repeat
+    background-size: 800px 1020px
+    border-bottom-left-radius: 2px
+    border-top-left-radius: 2px
+    box-sizing: border-box
+    color: variables.$color-text-default
+    display: inline-block
+    height: 2.1rem
+    max-width: 12rem
+    overflow: hidden
+    line-height: 1
+    text-transform: lowercase
+    padding: .25em 12px 0 5px
+    text-decoration: none
+    text-overflow: ellipsis
+    white-space: nowrap
+    margin: 0 4px 6px 0
+    &:hover
+      background-position: right -840px
+      color: variables.$color-tag-gray
+
+</style>
